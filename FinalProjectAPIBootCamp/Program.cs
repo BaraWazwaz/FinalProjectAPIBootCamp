e23c2b834e61075ac30fdd323d8e722d1fcd471f
BaraWazwaz <wazwazbaraa@gmail.com>
2026-02-11 18:19:19 +0200

    tried monster

--- FinalProjectAPIBootCamp/Program.cs
+++ FinalProjectAPIBootCamp/Program.cs
@@ -1,19 +1,23 @@
+using FinalProjectAPIBootCamp.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+string connectionString = "Server=db40980.databaseasp.net; Database=db40980; User Id=db40980; Password=3z_QX9e?6f!T; Encrypt=False; MultipleActiveResultSets=True;";
+builder.Services.AddDbContext<AppDbContext>(option => option.UseSqlServer(connectionString));
+
 var app = builder.Build();
 
-string connectionString = "";
-
-
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
